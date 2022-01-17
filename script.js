aeec59ed88a6515dfff654d2fadd2b4fce0a750a
NurbekUmirbek <91242146+NurbekUmirbek@users.noreply.github.com>
2022-01-17 12:46:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += 1000 ;
+       
+       
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= 1000;
+        
+    })
+  
+})
+$(document).ready(function() {
+    $('.block__title').click(function(event) {
+        if($('.block').hasClass('one')){
+            $('.block__title').not($(this)).removeClass('active');
+            $('.block__text').not($(this).next()).slideUp(300);
+        }
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+
+});
